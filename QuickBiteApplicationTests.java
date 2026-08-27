0d02028a9dd45e7ec26d5e3e80b79a22f0fa39dc
harshikaa20 <harshikaaviji@gmail.com>
2026-08-27 20:44:40 +0530

    Add files via upload

---
+++ QuickBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickbite.quickbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickBiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
